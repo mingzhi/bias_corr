06ea4366ef72bc5627d8b48d1d848e2de65d4f89
Mingzhi Lin <mingzhi9@gmail.com>
2016-07-29 00:26:04 -0400

    first commit

---
+++ bias_corr.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/alecthomas/kingpin"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"sort"
+	"time"
+)
+
+// Pop stores a population simulation results.
+type Pop struct {
+	Size, Length               int
+	MutationRate, TransferRate float64
+	FragLen                    int
+	Generation                 int
+	Genomes                    []string
+}
+
+func main() {
+	input := kingpin.Flag("input", "input population simulation results").Required().String()
+	output := kingpin.Flag("output", "output").Required().String()
+	sampleSize := kingpin.Flag("sample_size", "sample size").Default("10").Int()
+	clusterSize := kingpin.Flag("cluster_size", "cluster size").Default("2").Int()
+	ncpu := kingpin.Flag("ncpu", "number of cpus").Default("1").Int()
+	maxLen := kingpin.Flag("maxl", "max len of correlations").Default("100").Int()
+	bias := kingpin.Flag("bias", "bias sampling").Default("false").Bool()
+
+	kingpin.Parse()
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	popChan := readPops(*input)
+	resChan := make(chan Result)
+	done := make(chan bool)
+	for i := 0; i < *ncpu; i++ {
+		go func() {
+			for p := range popChan {
+				genomes := choose(p, *sampleSize, *clusterSize, *bias)
+				results := calcCorr(genomes, *maxLen)
+				for _, r := range results {
+					resChan <- r
+				}
+			}
+			done <- true
+		}()
+	}
+
+	go func() {
+		defer close(resChan)
+		for i := 0; i < *ncpu; i++ {
+			<-done
+		}
+	}()
+
+	results := collect(resChan, *maxLen)
+	write(results, *output)
+
+}
+
+// Tuble stores index and value.
+type Tuble struct {
+	index int
+	value float64
+}
+
+// Tubles is an array of Tuble
+type Tubles []Tuble
+
+// Len return the length of Tubles
+func (s Tubles) Len() int { return len(s) }
+
+// Swap swap the positions.
+func (s Tubles) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+// ByValue is a wrapper.
+type ByValue struct{ Tubles }
+
+// Less return true if i is less than j.
+func (s ByValue) Less(i, j int) bool { return s.Tubles[i].value < s.Tubles[j].value }
+
+func readPops(file string) chan Pop {
+	c := make(chan Pop)
+	go func() {
+		defer close(c)
+		f, err := os.Open(file)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		decoder := json.NewDecoder(f)
+		for {
+			var p Pop
+			if err := decoder.Decode(&p); err != nil {
+				if err != io.EOF {
+					panic(err)
+				}
+				break
+			}
+			c <- p
+		}
+	}()
+	return c
+}
+
+func choose(p Pop, sampleSize, clusterSize int, bias bool) (genomes []string) {
+	if bias {
+		return biasChoose(p, sampleSize, clusterSize)
+	}
+
+	return randomChoose(p, 50)
+}
+
+func randomChoose(p Pop, sampleSize int) (genomes []string) {
+	for i := 0; i < sampleSize; i++ {
+		index := rand.Intn(p.Size)
+		genomes = append(genomes, p.Genomes[index])
+	}
+
+	return
+}
+
+func biasChoose(p Pop, sampleSize, clusterSize int) (genomes []string) {
+	clusters := []int{}
+	for i := 0; i < clusterSize; i++ {
+		clusters = append(clusters, sampleSize)
+	}
+	clusters = append(clusters, 50)
+
+	indices := []int{}
+	for k := 0; k < len(clusters); k++ {
+		sampleSize = clusters[k]
+		central := rand.Intn(len(p.Genomes))
+		distances := calcDistances(p, central)
+		tubles := make(Tubles, len(distances))
+		for i := range distances {
+			tubles[i] = Tuble{index: i, value: distances[i]}
+		}
+		sort.Sort(ByValue{tubles})
+
+		for i := 0; i < sampleSize; i++ {
+			index := tubles[i].index
+			indices = append(indices, index)
+		}
+	}
+
+	for _, i := range indices {
+		genomes = append(genomes, p.Genomes[i])
+	}
+
+	return
+}
+
+func calcDistances(p Pop, i int) []float64 {
+	distances := []float64{}
+	for j := 0; j < len(p.Genomes); j++ {
+		d := 0.0
+		if i != j {
+			d = calcDist(p.Genomes[i], p.Genomes[j])
+		}
+		distances = append(distances, d)
+	}
+
+	return distances
+}
+
+func calcDist(a, b string) float64 {
+	d := 0
+	for i := range a {
+		if a[i] != b[i] {
+			d++
+		}
+	}
+	return float64(d) / float64(len(a))
+}
+
+func calcCorr(genomes []string, maxl int) []Result {
+	results := calcCm(genomes, maxl)
+	return results
+}
+
+// collect averages correlation results.
+func collect(resChan chan Result, maxLen int) map[string][]*MeanVar {
+	resMap := make(map[string][]*MeanVar)
+	for res := range resChan {
+		for len(resMap[res.Type]) <= res.Lag {
+			resMap[res.Type] = append(resMap[res.Type], NewMeanVar())
+		}
+		if !math.IsNaN(res.Value) {
+			resMap[res.Type][res.Lag].Add(res.Value)
+		}
+	}
+
+	return resMap
+}
+
+// write the final result.
+func write(result map[string][]*MeanVar, outFile string) {
+	w, err := os.Create(outFile)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	w.WriteString("l,m,v,n,t\n")
+	for t, mvs := range result {
+		for i := 0; i < len(mvs); i++ {
+			m := mvs[i].Mean()
+			v := mvs[i].Variance()
+			n := mvs[i].N
+			if n > 0 && !math.IsNaN(v) {
+				w.WriteString(fmt.Sprintf("%d", i))
+				w.WriteString(fmt.Sprintf(",%g,%g", m, v))
+				w.WriteString(fmt.Sprintf(",%d,%s\n", n, t))
+			}
+		}
+	}
+}
+
+// MeanVar is for calculate mean and variance in the increment way.
+type MeanVar struct {
+	N             int     // number of values.
+	M1            float64 // first moment.
+	Dev           float64
+	NDev          float64
+	M2            float64 // second moment.
+	BiasCorrected bool
+}
+
+// NewMeanVar return a new MeanVar.
+func NewMeanVar() *MeanVar {
+	return &MeanVar{}
+}
+
+// Add adds a value.
+func (m *MeanVar) Add(v float64) {
+	if m.N < 1 {
+		m.M1 = 0
+		m.M2 = 0
+	}
+
+	m.N++
+	n0 := m.N
+	m.Dev = v - m.M1
+	m.NDev = m.Dev / float64(n0)
+	m.M1 += m.NDev
+	m.M2 += float64(m.N-1) * m.Dev * m.NDev
+}
+
+// Mean returns the mean result.
+func (m *MeanVar) Mean() float64 {
+	return m.M1
+}
+
+// Variance returns the variance.
+func (m *MeanVar) Variance() float64 {
+	if m.N < 2 {
+		return math.NaN()
+	}
+
+	if m.BiasCorrected {
+		return m.M2 / float64(m.N-1)
+	}
+
+	return m.M2 / float64(m.N)
+}
+
+// Append add another result.
+func (m *MeanVar) Append(m2 *MeanVar) {
+	if m.N == 0 {
+		m.N = m2.N
+		m.M1 = m2.M1
+		m.Dev = m2.Dev
+		m.NDev = m2.NDev
+		m.M2 = m2.M2
+	} else {
+		if m2.N > 0 {
+			total1 := m.M1 * float64(m.N)
+			total2 := m2.M1 * float64(m2.N)
+			newMean := (total1 + total2) / float64(m.N+m2.N)
+			delta1 := m.Mean() - newMean
+			delta2 := m2.Mean() - newMean
+			sm := (m.M2 + m2.M2) + float64(m.N)*delta1*delta1 + float64(m2.N)*delta2*delta2
+			m.M1 = newMean
+			m.M2 = sm
+			m.N = m.N + m2.N
+		}
+	}
+}
---
+++ by_row.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"sort"
+)
+
+type Result struct {
+	Lag   int
+	Value float64
+	N     int
+	Type  string
+}
+
+// Sub records the position and
+type Sub struct {
+	Pos int
+	A   byte
+}
+
+// Subs is a list of Sub.
+type Subs []Sub
+
+// Len returns the length of Subs.
+func (s Subs) Len() int { return len(s) }
+
+// Swap swap the values at two positions.
+func (s Subs) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+// BySubPos is a wrapper for sorting.
+type BySubPos struct{ Subs }
+
+// Less return true if the value at i is less than the value at j.
+func (s BySubPos) Less(i, j int) bool {
+	return s.Subs[i].Pos < s.Subs[j].Pos
+}
+
+func calcCm(genomes []string, maxl int) (results []Result) {
+	subsArr := identifySubs(genomes)
+	length := len(genomes[0])
+
+	totals := make([]float64, maxl)
+	for i := 0; i < len(subsArr); i++ {
+		for j := i + 1; j < len(subsArr); j++ {
+			allSubs := removeDuplicateSubs(subsArr[i], subsArr[j])
+			positions := []int{}
+			for _, s := range allSubs {
+				positions = append(positions, s.Pos)
+			}
+
+			xy := make([]int, maxl)
+			for k := 0; k < len(positions); k++ {
+				for h := 0; h < len(positions); h++ {
+					lag := (positions[h] - positions[k] + length) % length
+					xy[lag]++
+				}
+			}
+
+			totalSubs := len(positions)
+			xbar := float64(totalSubs) / float64(length)
+			ybar := xbar
+			xbarybar := xbar * ybar
+
+			for lag := 0; lag < maxl; lag++ {
+				v := float64(xy[lag])/float64(length) - xbarybar
+				totals[lag] += v
+			}
+		}
+	}
+
+	n := len(subsArr) * (len(subsArr) - 1) / 2
+
+	for i := 0; i < maxl; i++ {
+		res := Result{}
+		res.Lag = i
+		res.N = n
+		res.Type = "Cm"
+		res.Value = totals[i] / float64(n)
+		results = append(results, res)
+	}
+
+	return
+}
+
+func identifySubs(genomes []string) (subsArr []Subs) {
+	ref := genomes[0]
+	subsArr = append(subsArr, Subs{})
+	for i := 1; i < len(genomes); i++ {
+		subs := Subs{}
+		for k := 0; k < len(ref); k++ {
+			if ref[k] != genomes[i][k] {
+				subs = append(subs, Sub{Pos: k, A: genomes[i][k]})
+			}
+		}
+		subsArr = append(subsArr, subs)
+	}
+
+	return
+}
+
+// removeDuplicateSubs
+func removeDuplicateSubs(subs1 Subs, others ...Subs) Subs {
+	allSubs := Subs{}
+	allSubs = append(allSubs, subs1...)
+	for _, subs := range others {
+		allSubs = append(allSubs, subs...)
+	}
+	if len(allSubs) <= 1 {
+		return allSubs
+	}
+	// remove same subsistutions.
+	sort.Sort(BySubPos{allSubs})
+
+	dedupSubs := Subs{}
+	old := Sub{Pos: allSubs[0].Pos - 1, A: ' '}
+	for _, s := range allSubs {
+		if s.Pos == old.Pos {
+			if s.A == old.A {
+				dedupSubs = dedupSubs[:len(dedupSubs)-1]
+			}
+		} else {
+			dedupSubs = append(dedupSubs, s)
+		}
+		old = s
+	}
+
+	return dedupSubs
+}
+
+// removeDuplicateInts
+func removeDuplicateInts(values []int, others ...[]int) []int {
+	all := []int{}
+	others = append(others, values)
+	for _, vs := range others {
+		all = append(all, vs...)
+	}
+
+	if len(all) <= 1 {
+		return all
+	}
+
+	sort.Ints(all)
+	dedupInts := []int{}
+	old := all[0] - 1
+	for _, s := range all {
+		if s == old {
+			dedupInts = dedupInts[:len(dedupInts)-1]
+		} else {
+			dedupInts = append(dedupInts, s)
+		}
+		old = s
+	}
+
+	return dedupInts
+}
